b13568e463c54f7f8220e3d5fea6ad4faf653df2
Eric Bruch <eric.bruch@outlook.de>
2022-12-17 17:11:07 +0100

    tidy up code

--- join_backend_api/core/models.py
+++ join_backend_api/core/models.py
@@ -2,10 +2,15 @@ from django.db import models
 import datetime
 from dateutil.relativedelta import *
 
-# MYStatus = Enum('Status', ['BACKLOG', 'TO_DO', 'IN_PROGRESS', 'REVIEW', 'DONE'])
 # class Status(models.TextChoices):
 #     BACKLOG = 'BL'
 #     TO_DO = 'TD'
+
+Urgency = models.TextChoices('Urgency', ['URGENT', 'MEDIUM', 'LOW'])
+
+Status = models.TextChoices(
+    'Status', ['BACKLOG', 'TO_DO', 'IN_PROGRESS', 'REVIEW', 'DONE']
+)
 
 
 class Board(models.Model):
@@ -22,22 +27,16 @@ class Task(models.Model):
     )
     created_at = models.DateField(default=datetime.datetime.now())
     board = models.ForeignKey('Board', on_delete=models.CASCADE)
-
     parent = models.ForeignKey(
         'self', blank=True, null=True, related_name='children', on_delete=models.CASCADE
     )
-    # subtask = models.ManyToManyField('self', blank=True)
 
-    Urgency = models.TextChoices('Urgency', ['URGENT', 'MEDIUM', 'LOW'])
     urgency = models.CharField(
         choices=Urgency.choices,
         max_length=20,
         default=Urgency.MEDIUM,
     )
 
-    Status = models.TextChoices(
-        'Status', ['BACKLOG', 'TO_DO', 'IN_PROGRESS', 'REVIEW', 'DONE']
-    )
     status = models.CharField(
         choices=Status.choices,
         max_length=20,
