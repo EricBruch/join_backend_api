768d8afd41675db6b645b3665ea1436fa5e41df9
Eric Bruch <eric.bruch@outlook.de>
2022-12-17 16:23:23 +0100

    WIP

--- join_backend_api/core/models.py
+++ join_backend_api/core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from dateutil import relativedelta
-from enum import Enum
+import datetime
+from dateutil.relativedelta import *
 
 # MYStatus = Enum('Status', ['BACKLOG', 'TO_DO', 'IN_PROGRESS', 'REVIEW', 'DONE'])
 # class Status(models.TextChoices):
@@ -10,22 +10,32 @@ from enum import Enum
 
 class Board(models.Model):
     title = models.CharField(max_length=100, default='')
-    created_at = models.DateField(default=date.today())
+    created_at = models.DateField(default=datetime.datetime.now())
 
 
 class Task(models.Model):
     title = models.CharField(max_length=100, default='')
     category = models.CharField(max_length=20, default='')
     description = models.CharField(max_length=300, default='')
-    # due_date = models.DateField(
-    #     default=(datetime.today + relativedelta(months=+3))
-    # )
-    created_at = models.DateField(default=date.today())
+    due_date = models.DateField(
+        default=(datetime.datetime.now() + relativedelta(months=+3))
+    )
+    created_at = models.DateField(default=datetime.datetime.now())
+    # board = models.ForeignKey('Board', on_delete=models.CASCADE)
+
+    # parent = models.ForeignKey('Task', on_delete=models.CASCADE)
+    # subtask = models.ManyToManyField('self', blank=True)
+
+    Urgency = models.TextChoices('Urgency', ['URGENT', 'MEDIUM', 'LOW'])
+    urgency = models.CharField(
+        choices=Urgency.choices,
+        max_length=20,
+        default=Urgency.MEDIUM,
+    )
 
     Status = models.TextChoices(
         'Status', ['BACKLOG', 'TO_DO', 'IN_PROGRESS', 'REVIEW', 'DONE']
     )
-
     status = models.CharField(
         choices=Status.choices,
         max_length=20,
