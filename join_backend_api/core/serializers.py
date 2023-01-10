d383c7075df98d7528977560775e9af60f35a33f
Eric Bruch <eric.bruch@outlook.de>
2023-01-10 21:35:49 +0100

    adapt code

--- join_backend_api/core/serializers.py
+++ join_backend_api/core/serializers.py
@@ -2,9 +2,12 @@ from rest_framework import serializers
 from .models import Board, Task, TaskComment, TaskAssignedUser
 from accounts.models import CustomUser
 from django.utils.timezone import now
+from .utils import create_and_get_Task, create_task_comments, create_task_assigned_users, getMappedTaskInstance
 
 
 class BoardSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateField(required=False, default=now().date)
+
     class Meta:
         model = Board
         fields = ['id', 'title', 'created_at']
@@ -94,62 +97,6 @@ class TaskSerializer(serializers.ModelSerializer):
         return task
 
     def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.author = validated_data.pop('author', instance.author)
-        instance.category = validated_data.pop('category', instance.category)
-        instance.description = validated_data.pop(
-            'description', instance.description
-        )
-        instance.due_date = validated_data.pop('due_date', instance.due_date)
-        instance.created_at = validated_data.pop(
-            'created_at', instance.created_at
-        )
-        instance.board = validated_data.pop('board', instance.board)
-        instance.parent = validated_data.pop('parent', instance.parent)
-        instance.urgency = validated_data.pop('urgency', instance.urgency)
-        instance.status = validated_data.pop('status', instance.status)
-
-        instance.save()
-        return instance
-
-
-def create_and_get_Task(validated_data):
-    task = Task.objects.create(
-        title=validated_data.pop('title'),
-        author=validated_data.pop('author'),
-        category=validated_data.pop('category'),
-        description=validated_data.pop('description'),
-        due_date=validated_data.pop('due_date'),
-        created_at=now_or_date(validated_data.pop('created_at')),
-        board=validated_data.pop('board'),
-        parent=validated_data.pop('parent'),
-        urgency=validated_data.pop('urgency'),
-        status=validated_data.pop('status')
-    )
-    task.save()
-    return task
-
-
-def create_task_comments(comments, task):
-    for c in comments:
-        newComment = TaskComment.objects.create(
-            user=c.get('user'),
-            task=task,
-            text=c.get('text'),
-            created_at=now_or_date(c.get('created_at'))
-        )
-        newComment.save()
-
-
-def create_task_assigned_users(assignedUsers, task):
-    for aU in assignedUsers:
-        newAssignedUser = TaskAssignedUser.objects.create(
-            user=aU.get('user'),
-            task=task,
-            created_at=now_or_date(aU.get('created_at'))
-        )
-        newAssignedUser.save()
-
-
-def now_or_date(date):
-    return now() if date is None else date
+        mappedInstance = getMappedTaskInstance(instance, validated_data)
+        mappedInstance.save()
+        return mappedInstance
