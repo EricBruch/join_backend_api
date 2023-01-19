d66e49184d3b27e29ec8a047efce8f65f0059e8f
Eric Bruch <eric.bruch@outlook.de>
2023-01-19 21:50:46 +0100

    add capability for browsable api to generate content

--- join_backend_api/core/urls.py
+++ join_backend_api/core/urls.py
@@ -3,13 +3,14 @@ from rest_framework.routers import DefaultRouter
 from .views import BoardViewSet, TaskViewSet, CommentsViewSet, AssignedUsersViewSet
 
 router = DefaultRouter()
-router.register(r'boards', BoardViewSet, basename="boards")
-router.register(r'tasks', TaskViewSet, basename="tasks")
-router.register(r'comments', CommentsViewSet, basename='comments')
-router.register(r'assignedUsers', AssignedUsersViewSet, basename='assignedUsers')
+router.register(r'boards', BoardViewSet, basename="board")
+router.register(r'tasks', TaskViewSet, basename="task")
+router.register(r'comments', CommentsViewSet, basename='taskcomment')
+router.register(r'assignedUsers', AssignedUsersViewSet, basename='taskassigneduser')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
 ]
