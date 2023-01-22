0f41988c1e1b9b21176074b3dbdee0ad52d86478
Eric Bruch <eric.bruch@outlook.de>
2023-01-22 15:29:08 +0100

    remove not used api_view

--- join_backend_api/core/views.py
+++ join_backend_api/core/views.py
@@ -11,12 +11,12 @@ from rest_framework.decorators import api_view
 from rest_framework.reverse import reverse
 
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'boards': reverse('board-list', request=request, format=format),
-        'tasks': reverse('task-list', request=request, format=format)
-    })
+# @api_view(['GET'])
+# def api_root(request, format=None):
+#     return Response({
+#         'boards': reverse('board-list', request=request, format=format),
+#         'tasks': reverse('task-list', request=request, format=format)
+#     })
 
 
 class BoardViewSet(viewsets.ModelViewSet):
