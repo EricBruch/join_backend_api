483dea02f2f366652a7c48a4233d07d11f7d88c5
Eric Bruch <eric.bruch@outlook.de>
2023-01-28 11:33:55 +0100

    %*

--- join_backend_api/core/tests.py
+++ join_backend_api/core/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class BasicTest(APITestCase):
+    def test_get_a_board_list(self):
+        response = self.client.get('/boards/')
+        self.assertEqual(type(response.json()['results']), list)
