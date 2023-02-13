90b2eb780d29727d9b3b297d38e47c3a6d8ea811
Eric Bruch <eric.bruch@outlook.de>
2023-02-13 20:24:09 +0100

    integrate comments form code review for possible improvement

--- join_backend_api/core/models.py
+++ join_backend_api/core/models.py
@@ -6,34 +6,28 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-Urgency = models.TextChoices('Urgency', ['URGENT', 'MEDIUM', 'LOW'])
+Urgency = models.TextChoices("Urgency", ["URGENT", "MEDIUM", "LOW"])
 
 Status = models.TextChoices(
-    'Status', ['BACKLOG', 'TO_DO', 'IN_PROGRESS', 'REVIEW', 'DONE']
+    "Status", ["BACKLOG", "TO_DO", "IN_PROGRESS", "REVIEW", "DONE"]
 )
 
 
 class Board(models.Model):
-    title = models.CharField(max_length=100, default='')
+    title = models.CharField(max_length=100, default="")
     created_at = models.DateField(default=now())
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=100, default='')
+    title = models.CharField(max_length=100, default="")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.CharField(max_length=20, default='')
-    description = models.CharField(max_length=300, default='')
-    due_date = models.DateField(
-        default=(now() + relativedelta(months=+3))
-    )
+    category = models.CharField(max_length=20, default="")
+    description = models.CharField(max_length=300, default="")
+    due_date = models.DateField(default=(now() + relativedelta(months=+3)))
     created_at = models.DateField(default=now())
-    board = models.ForeignKey('Board', on_delete=models.CASCADE)
+    board = models.ForeignKey("Board", on_delete=models.CASCADE)
     parent = models.ForeignKey(
-        'self',
-        blank=True,
-        null=True,
-        related_name='children',
-        on_delete=models.CASCADE
+        "self", blank=True, null=True, related_name="children", on_delete=models.CASCADE
     )
     urgency = models.CharField(
         choices=Urgency.choices,
@@ -41,17 +35,17 @@ class Task(models.Model):
         default=Urgency.MEDIUM,
     )
     status = models.CharField(
-        choices=Status.choices,
-        max_length=20,
-        default=Status.BACKLOG
+        choices=Status.choices, max_length=20, default=Status.BACKLOG
     )
     comments = models.ManyToManyField(
-        User, through='TaskComment', related_name='taksOfUser')
+        User, through="TaskComment", related_name="taksOfUser"
+    )
     assignedUsers = models.ManyToManyField(
-        User, through='TaskAssignedUser', related_name='tasksOfUser'
+        User, through="TaskAssignedUser", related_name="tasksOfUser"
     )
 
 
+# can be one to Many relationship
 class TaskComment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
--- join_backend_api/core/utils.py
+++ join_backend_api/core/utils.py
@@ -8,16 +8,16 @@ def now_or_date(date):
 
 def create_and_get_Task(validated_data):
     task = Task.objects.create(
-        title=validated_data.pop('title'),
-        author=validated_data.pop('author'),
-        category=validated_data.pop('category'),
-        description=validated_data.pop('description'),
-        due_date=validated_data.pop('due_date'),
-        created_at=now_or_date(validated_data.pop('created_at')),
-        board=validated_data.pop('board'),
-        parent=validated_data.pop('parent'),
-        urgency=validated_data.pop('urgency'),
-        status=validated_data.pop('status')
+        title=validated_data.pop("title"),
+        author=validated_data.pop("author"),
+        category=validated_data.pop("category"),
+        description=validated_data.pop("description"),
+        due_date=validated_data.pop("due_date"),
+        created_at=now_or_date(validated_data.pop("created_at")),
+        board=validated_data.pop("board"),
+        parent=validated_data.pop("parent"),
+        urgency=validated_data.pop("urgency"),
+        status=validated_data.pop("status"),
     )
     task.save()
     return task
@@ -26,38 +26,32 @@ def create_and_get_Task(validated_data):
 def create_task_comments(comments, task):
     for c in comments:
         newComment = TaskComment.objects.create(
-            user=c.get('user'),
+            user=c.get("user"),
             task=task,
-            text=c.get('text'),
-            created_at=now_or_date(c.get('created_at'))
+            text=c.get("text"),
+            created_at=now_or_date(c.get("created_at")),
         )
         newComment.save()
 
 
-def create_task_assigned_users(assignedUsers, task):
+def create_assigned_users(assignedUsers, task):
     for aU in assignedUsers:
         newAssignedUser = TaskAssignedUser.objects.create(
-            user=aU.get('user'),
-            task=task,
-            created_at=now_or_date(aU.get('created_at'))
+            user=aU.get("user"), task=task, created_at=now_or_date(aU.get("created_at"))
         )
         newAssignedUser.save()
 
 
 def getMappedTaskInstance(instance, validated_data):
-    instance.title = validated_data.get('title', instance.title)
-    instance.author = validated_data.pop('author', instance.author)
-    instance.category = validated_data.pop('category', instance.category)
-    instance.description = validated_data.pop(
-        'description', instance.description
-    )
-    instance.due_date = validated_data.pop('due_date', instance.due_date)
-    instance.created_at = validated_data.pop(
-        'created_at', instance.created_at
-    )
-    instance.board = validated_data.pop('board', instance.board)
-    instance.parent = validated_data.pop('parent', instance.parent)
-    instance.urgency = validated_data.pop('urgency', instance.urgency)
-    instance.status = validated_data.pop('status', instance.status)
+    instance.title = validated_data.get("title", instance.title)
+    instance.author = validated_data.pop("author", instance.author)
+    instance.category = validated_data.pop("category", instance.category)
+    instance.description = validated_data.pop("description", instance.description)
+    instance.due_date = validated_data.pop("due_date", instance.due_date)
+    instance.created_at = validated_data.pop("created_at", instance.created_at)
+    instance.board = validated_data.pop("board", instance.board)
+    instance.parent = validated_data.pop("parent", instance.parent)
+    instance.urgency = validated_data.pop("urgency", instance.urgency)
+    instance.status = validated_data.pop("status", instance.status)
 
     return instance
